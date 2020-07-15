045668d0b49baac37b546904627f472a4468e435
Kasper Vestergaard <kav@goaskcody.com>
2020-07-15 14:15:44 +0200

    Working resolvers

--- start/server/src/index.js
+++ start/server/src/index.js
@@ -1,8 +1,26 @@
 const { ApolloServer } = require('apollo-server');
 const typeDefs = require('./schema');
+const { createStore } = require('./utils');
+const resolvers = require('./resolvers');
+const store = createStore();
 
-const server = new ApolloServer({ typeDefs });
+const server = new ApolloServer(
+    {
+        typeDefs,
+        resolvers,
+        dataSources: () => (
+            {
+                launchAPI: new LaunchAPI(),
+                userAPI: new UserAPI(
+                    { store }
+                )
+            }
+        )
+    }
+);
 
-server.listen().then(({ url }) => {
-    console.log(`🚀 Server ready at ${url}`);
-  });
+server.listen().then(
+    ({ url }) => {
+        console.log(`🚀 Server ready at ${url}`);
+    }
+);
